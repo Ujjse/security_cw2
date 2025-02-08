94053fd9fa5a80c7d394faae9e9787b303ffd742
Ujjse <220003@softwarica.edu.np>
2025-02-08 13:25:53 +0545

    api

---
+++ frontend/src/apis/api.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+// Creating an instance of axios
+const Api = axios.create({
+    baseURL : "http://localhost:5000",
+    withCredentials : true,
+    headers : {
+        "Content-Type" : "application/form-data"
+    }
+});
+
+// Creating test api
+export const testApi = () => Api.get('/test')
+export const newTestApi = () => Api.get('/test_new')
+
+// Creating register api\
+export const registerUserApi = (data) => Api.post('/api/user/register', data)
+
+// cresting login api
+export const loginUserApi = (data) => Api.post('/api/user/login',data)
+
+// create product create api
+export const createProductApi = (data) => Api.post('/api/product/create', data)
+
+
+// http://localhost:5500/test
